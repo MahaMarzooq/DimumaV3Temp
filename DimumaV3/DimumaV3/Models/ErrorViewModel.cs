78a874e1c29b3d40e82c1226835238855b2e50c9
MahaMarzooq <93163121+MahaMarzooq@users.noreply.github.com>
2025-02-18 12:31:59 +0300

    Add files via upload

---
+++ DimumaV3/DimumaV3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DimumaV3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
