20e6d99b50fdf425ad3c65cd1b4b37c6d2a11f67
MahaMarzooq <93163121+MahaMarzooq@users.noreply.github.com>
2025-02-18 12:39:16 +0300

    Delete DimumaV3 directory

--- DimumaV3/DimumaV3/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DimumaV3.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
